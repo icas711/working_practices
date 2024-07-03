091df20d4df449ccc4d8b6504f19aded0fb34ac3
icas711 <50333299+icas711@users.noreply.github.com>
2024-07-03 11:15:24 +0300

    mvvm_authentication

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,16 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
-import 'package:working_practices/app.dart';
-import 'package:working_practices/app/router.dart';
 
 import 'package:working_practices/main.dart';
+import 'package:working_practices/ui/widgets/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    final GoRouter router = RoutingService().router;
-    await tester.pumpWidget(App(router: router));
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
